a738657f1f9859697f8bd84fd4afd67d16af0754
James <jmz@buildbox.(none)>
2013-03-09 13:11:49 -0500

    3.0.27

--- build.sh
+++ build.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-rm output/system/lib/modules/*
+export USE_CCACHE=1
+export CCACHE_DIR=/media/out/.ccache
+ccache -M 60G
+rm output/files/system/lib/modules/*
+rm output/files/boot.img
+rm output/boot.img-ramdisk.gz
 make clean
 make mrproper
 make tc2_defconfig
 make -j9
 mkdir output
-mkdir output/system
-mkdir output/system/lib
-mkdir output/system/lib/modules
-find . -name '*.ko' -exec cp '{}' output/system/lib/modules \;
+mkdir output/files/system
+mkdir output/files/system/lib
+mkdir output/files/system/lib/modules
+find . -name '*.ko' -exec cp '{}' output/files/system/lib/modules \;
+cd output
+./mkbootfs ramdisk | gzip > boot.img-ramdisk.gz
+NOW=$(date +"%m-%d-%y_%H:%M")
+./mkbootimg --kernel ../arch/arm/boot/zImage --ramdisk boot.img-ramdisk.gz -o boot.img --base 80400000 --ramdisk_offset 81808000
+mv boot.img files/
+
+
